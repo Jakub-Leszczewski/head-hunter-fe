bb87406104ab0c9b9eaa555e3b11142fe3a578b1
Mateusz Derus <mateuszderus01@gmail.com>
2022-07-20 15:21:53 +0200

    bem class names

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,8 +1,13 @@
 interface Props {
   type: any;
   children: string;
+  className: string;
 }
 
-export const Button = ({type, children}: Props) => {
-  return <button type={type}>{children}</button>;
+export const Button = ({type, children, className}: Props) => {
+  return (
+    <button type={type} className={className}>
+      {children}
+    </button>
+  );
 };
--- src/components/views/Login/Login.tsx
+++ src/components/views/Login/Login.tsx
@@ -5,19 +5,16 @@ import {Input} from '../../common/Input/Input';
 export const Login = () => {
   return (
     <form className='login'>
-      <img src='/images/logo.png' alt='MegaK logo' />
-      <Input className='input login-input' placeholder='E-mail' type='email' />
-      <Input
-        className='input password-input'
-        placeholder='Hasło'
-        type='password'
-      />
-      <Link to='/'>Zapomniałeś hasła?</Link>
-      <div>
-        <p>
-          Nie masz konta? <Link to='/'>Zarejestruj się</Link>
-        </p>
-        <Button type='submit'>Zaloguj się</Button>
+      <img className='login__image' src='/images/logo.png' alt='MegaK logo' />
+      <Input className='login__input' placeholder='E-mail' type='email' />
+      <Input className='login__input' placeholder='Hasło' type='password' />
+      <div className='login__container'>
+        <Link className='login__link' to='/'>
+          Zapomniałeś hasła?
+        </Link>
+        <Button type='submit' className='login__btn'>
+          Zaloguj się
+        </Button>
       </div>
     </form>
   );
