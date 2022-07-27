10ea05b793ed1dc61215a08e5acc4c0807c777a2
florczak_arek <florczakarek@gmail.com>
2022-07-27 13:08:44 +0200

    Add NewPassword view, add new icon in Forgot view, fixed forgot.scss into bem terminology

--- src/components/views/ForgotPassword/Forgot.tsx
+++ src/components/views/ForgotPassword/Forgot.tsx
@@ -1,21 +1,21 @@
 import { FormEvent, useState } from 'react';
-import { FiKey } from 'react-icons/fi';
+import { GiThink } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
 import { Button } from '../../common/Button/Button';
 import { Input } from '../../common/Input/Input';
 
-interface Consumer {
+interface Email {
   email: string;
 }
 
 export const Forgot = () => {
-  const [consumer, setConsumer] = useState<Consumer>({
+  const [email, setEmail] = useState<Email>({
     email: '',
   });
 
-  const editConsumer = (name: string, value: string | number) => {
-    setConsumer((consumer) => ({
-      ...consumer,
+  const emailHandler = (name: string, value: string | number) => {
+    setEmail((email) => ({
+      ...email,
       [name]: value,
     }));
   };
@@ -26,35 +26,35 @@ export const Forgot = () => {
 
   return (
     <div className='forgot'>
-      <div className='forgot_icon'>
-        <FiKey className='forgot_icon_key' />
+      <div className='forgot__icon'>
+        <GiThink className='forgot__icon-key' />
       </div>
-      <div className='forgot_text'>
+      <div className='forgot__text'>
         <h2>Nie pamiętasz hasła?</h2>
         <small>
           Proszę wpisz adres email podany podczas rejestracji. Otrzymasz
           tymczasowy link do zresetowania hasła.
         </small>
       </div>
-      <form className='forgot_form' onSubmit={submitForgotPasswordHandler}>
-        <label htmlFor='email' className='forgot_form_label'>
+      <form className='forgot__form' onSubmit={submitForgotPasswordHandler}>
+        <label htmlFor='email' className='forgot__form-label'>
           Email
         </label>
         <Input
-          className='forgot_form_input'
+          className='forgot__form-input'
           name='email'
           type='email'
           placeholder='Wpisz swój email'
-          value={consumer.email}
-          change={editConsumer}
+          value={email.email}
+          change={emailHandler}
         />
         <Button
           type='submit'
           className='forgot_form_btn'
-          textName='Resetuj hasło'
+          textName='Wyślij link'
         />
       </form>
-      <Link to='/login' className='forgot_link'>
+      <Link to='/login' className='forgot__link'>
         ← Wróć do logowania
       </Link>
     </div>
---
+++ src/components/views/ForgotPassword/NewPassword.tsx
@@ -0,0 +1,73 @@
+import { FormEvent, useState } from 'react';
+import { FiKey } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Button } from '../../common/Button/Button';
+import { Input } from '../../common/Input/Input';
+
+interface NewPassword {
+  password: string;
+}
+
+export const NewPassword = () => {
+  const navigate = useNavigate();
+
+  const [newPassword, setNewPassword] = useState<NewPassword>({
+    password: '',
+  });
+
+  const newPasswordHandler = (name: string, value: string | number) => {
+    setNewPassword((password) => ({
+      ...password,
+      [name]: value,
+    }));
+  };
+
+  const submitForgotPasswordHandler = (e: FormEvent): void => {
+    e.preventDefault();
+    toast.success('Twoje hasło zostało pomyślnie zresetowane.');
+    navigate('/login');
+  };
+
+  return (
+    <div className='forgot'>
+      <div className='forgot__icon'>
+        <FiKey className='forgot__icon-key' />
+      </div>
+      <div className='forgot__text'>
+        <h2>Ustaw nowe hasło</h2>
+        <small>
+          Pamiętaj, ze Twoje nowe hasło musi być inne od poprzednio ustawionego.
+        </small>
+      </div>
+      <form className='forgot__form' onSubmit={submitForgotPasswordHandler}>
+        <label htmlFor='new-password' className='forgot__form-label'>
+          Hasło
+        </label>
+        <Input
+          className='forgot__form-input'
+          name='new-password'
+          type='password'
+          placeholder='Wpisz nowe hasło'
+          value={newPassword.password}
+          change={newPasswordHandler}
+        />
+        <label htmlFor='confirm_password' className='forgot_form_label'>
+          Potwierdź hasło
+        </label>
+        <Input
+          className='forgot__form-input'
+          name='confirm_password'
+          type='password'
+          placeholder='Potwierdź nowe hasło'
+          value={newPassword.password}
+          change={newPasswordHandler}
+        />
+        <Button type='submit' className='forgot__form-btn' textName='Resetuj' />
+      </form>
+      <Link to='/login' className='forgot__link'>
+        ← Wróć do logowania
+      </Link>
+    </div>
+  );
+};
