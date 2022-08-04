909bb53b13fc512dc4d01c28d54feabeedf66699
Mateusz Derus <mateuszderus01@gmail.com>
2022-08-04 21:16:56 +0200

    added preferences component & edit others

--- src/components/AvailableStudentsListItem/AvailableStudentsListItem.tsx
+++ src/components/AvailableStudentsListItem/AvailableStudentsListItem.tsx
@@ -1,10 +1,24 @@
 import React, {useState} from 'react';
 import {Button} from '../common/Button/Button';
 import {BsChevronDown, BsChevronUp} from 'react-icons/bs';
+import {AvailableStudentsPreferences} from '../AvailableStudentsPreferences/AvailableStudentsPreferences';
 
 interface Props {
   item: any;
 }
+
+const titles = {
+  courseRating: 'Ocena przejścia kursu',
+  activityRating: 'Ocena aktywności i zaangażowania na kursie',
+  codeRating: 'Ocena kodu w projekcie własnym',
+  scrumRating: 'Ocena pracy w zespole Scrum',
+  preferredPlace: 'Preferowane miejsce pracy',
+  city: 'Docelowe miasto, gdzie chce pracować kandydat',
+  contractType: 'Oczekiwany typ kontraktu',
+  salary: 'Oczekiwane wynagrodzenie miesięczne brutto',
+  internship: 'Zgoda na odbycie bezpłatnych praktyk, stażu na początek',
+  commercialExp: 'Komercyjne doświadczenie w programowaniu',
+};
 
 export const AvailableStudentsListItem = ({item}: Props) => {
   const [isStudentInfoOpen, setIsStudentInfoOpen] = useState(false);
@@ -24,101 +38,52 @@ export const AvailableStudentsListItem = ({item}: Props) => {
             textName={'Zarezerwuj rozmowę'}
           />
           {isStudentInfoOpen ? (
-            <BsChevronUp
-              onClick={handleIsInfoOpen}
-              style={{
-                color: '#666666',
-                fontSize: '20px',
-                cursor: 'pointer',
-              }}
-            />
+            <BsChevronUp onClick={handleIsInfoOpen} className='arrow-icon' />
           ) : (
-            <BsChevronDown
-              onClick={handleIsInfoOpen}
-              style={{
-                color: '#666666',
-                fontSize: '20px',
-                cursor: 'pointer',
-              }}
-            />
+            <BsChevronDown onClick={handleIsInfoOpen} className='arrow-icon' />
           )}
         </div>
       </div>
       {/* INFO */}
       {isStudentInfoOpen && (
         <div className='list-item-info-container'>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>Ocena przejścia kursu</p>
-            <p className='list-item-info__second'>
-              <strong>{item.courseRating}</strong> /5
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Ocena aktywności i zaangażowania na kursie
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.activityRating}</strong> /5
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Ocena kodu w projekcie własnym
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.codeRating}</strong> /5
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>Ocena pracy w zespole Scrum</p>
-            <p className='list-item-info__second'>
-              <strong>{item.scrumRating}</strong> /5
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>Preferowane miejsce pracy</p>
-            <p className='list-item-info__second'>
-              <strong>{item.preferredPlace}</strong>
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Docelowe miasto, gdzie chce pracować kandydat
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.city}</strong>
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>Oczekiwany typ kontraktu</p>
-            <p className='list-item-info__second'>
-              <strong>{item.contractType}</strong>
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Oczekiwane wynagrodzenie miesięczne brutto
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.salary}</strong>
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Zgoda na odbycie bezpłatnych praktyk, stażu na początek
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.internship ? 'TAK' : 'NIE'}</strong>
-            </p>
-          </div>
-          <div className='list-item-info'>
-            <p className='list-item-info__first'>
-              Komercyjne doświadczenie w programowaniu
-            </p>
-            <p className='list-item-info__second'>
-              <strong>{item.commercialExp}</strong>
-            </p>
-          </div>
+          <AvailableStudentsPreferences
+            titles={titles.courseRating}
+            val={item.courseRating}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.activityRating}
+            val={item.activityRating}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.codeRating}
+            val={item.codeRating}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.scrumRating}
+            val={item.scrumRating}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.preferredPlace}
+            val={item.preferredPlace}
+          />
+          <AvailableStudentsPreferences titles={titles.city} val={item.city} />
+          <AvailableStudentsPreferences
+            titles={titles.contractType}
+            val={item.contractType}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.salary}
+            val={item.salary}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.internship}
+            val={item.internship}
+          />
+          <AvailableStudentsPreferences
+            titles={titles.commercialExp}
+            val={item.commercialExp}
+          />
         </div>
       )}
       <div className='list-item-separator' />
