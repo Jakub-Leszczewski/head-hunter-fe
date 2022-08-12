02c08a75a2fbe1d0e2ef072310bfd6494933ef59
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-08-12 16:44:50 +0200

    HR registration

--- src/views/HRRegistration/HRRegistration.tsx
+++ src/views/HRRegistration/HRRegistration.tsx
@@ -1,91 +1,74 @@
-import React, { useState } from 'react';
-import { BsEyeSlash, BsEye } from 'react-icons/bs';
-
-import logo from '../../../assets/images/logo.png';
-import { Input } from '../../components/common/Input/Input'
-import { Label } from '../../components/common/Label/Label'
-import { Button } from '../../components/common/Button/Button'
+import React, { FormEvent, useState } from 'react';
+import logo from '../../assets/images/logo.png';
+import { Label } from '../../components/common/Label/Label';
+import { Button } from '../../components/common/Button/Button';
+import { fetchTool } from '../../utils/fetchHelpers';
+import { useNavigate, useParams } from 'react-router-dom';
+import { InputPassword } from '../../components/common/InputPassword/InputPassword';
 
 interface Passwords {
   password: string;
   confirmPassword: string;
-  passwordView: boolean;
-  confirmPasswordView: boolean;
 }
 
 export const HRRegistration = () => {
 
+  const { token } = useParams();
+  const navigate = useNavigate();
+
   const [passwords, setPasswords] = useState<Passwords>({
     password: '',
     confirmPassword: '',
-    passwordView: false,
-    confirmPasswordView: false,
-  })
+  });
+  const { confirmPassword, password } = passwords;
 
   const changePassword = (name: string, value: string | number | boolean) => {
     (setPasswords(passwords => ({
-        ...passwords,
-        [name]: value,
-      }))
+      ...passwords,
+      [name]: value,
+    }))
     )
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) return console.log('Podano rózne hasła.');
+    const response = await fetchTool(`hr/${token}`, 'PATCH', { newPassword: password });
+    if (!response.status) return console.log('Coś się popsuło i nie było mnie słychać');
+    console.log('Zarejestrowano HRa');
+    navigate('/login');
   };
 
   return (
     <div className="hr-registration">
-      <img className="hr-registration__image" src={logo} alt="MegaK logo"/>
+      <img className="hr-registration__image" src={logo} alt="MegaK logo" />
       <h2 className="hr-registration__title">
         Rejestracja HR-a
       </h2>
-      <form className="hr-registration__form">
+      <form className="hr-registration__form" onSubmit={handleSubmit}>
         <div className="hr-registration__form-input">
           <Label
             htmlFor="password"
             textName="Hasło"
           />
-          <Input
+          <InputPassword
             name="password"
-            value={passwords.password}
-            change={changePassword}
-            type={passwords.passwordView ? 'text' : 'password'}
+            changePassword={changePassword}
+            password={password}
+          // className=""
           />
-          <div
-            className="hr-registration__form-icon-box"
-            onClick={() => changePassword('passwordView', !passwords.passwordView)}
-          >
-            {passwords.passwordView
-              ? <BsEyeSlash
-                className="hr-registration__form-icon"
-              />
-              : <BsEye
-                className="hr-registration__form-icon"
-              />
-            }
-          </div>
         </div>
         <div className="hr-registration__form-input">
           <Label
             htmlFor="confirmPassword"
             textName="Powtórz hasło"
           />
-          <Input
+          <InputPassword
             name="confirmPassword"
-            value={passwords.confirmPassword}
-            change={changePassword}
-            type={passwords.confirmPasswordView ? 'text' : 'password'}
+            changePassword={changePassword}
+            password={confirmPassword}
+          // className=""
           />
-          <div
-            className="hr-registration__form-icon-box"
-            onClick={() => changePassword('confirmPasswordView', !passwords.confirmPasswordView)}
-          >
-            {passwords.confirmPasswordView
-              ? <BsEyeSlash
-                className="hr-registration__form-icon"
-              />
-              : <BsEye
-                className="hr-registration__form-icon"
-              />
-            }
-          </div>
         </div>
 
         <div className="hr-registration__form-button">
